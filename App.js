1ca860f776e4d07bb726d8a5ac602cb9bb8bb14d
Alka Chauhan <alkachauhan492@gmail.com>
2024-06-09 15:40:42 +0530

    completed level 1

---
+++ App.js
@@ -0,0 +1,21 @@
+/*
+<div id="parent">
+<div if="child">
+<h1>i am an h1 tag</h1>
+</div>
+</div>
+
+*/
+
+// React.createElement(object)=>HTML(Browser understanding)
+
+const parent=React.createElement('div',{id:'parent'},React.createElement('div',{id:"child"},
+       [ React.createElement('h1',{},'i am an h1 tag'),React.createElement('h2',{},'i am an h2 tag')])
+    
+)
+const heading=React.createElement(
+    'h1',{
+        "id":"heading"
+    },"Hello World from React!");
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
